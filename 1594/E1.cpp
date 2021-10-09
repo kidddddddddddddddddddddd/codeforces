4e457a332c5973237e3be5247e7031a05c3e1b28
kidddddddddddddddddddddd <1062602710@qq.com>
2021-10-09 15:05:00 +0800

    1594 E1

---
+++ 1594/E1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+const int maxn = 1e6 + 7;
+const int mod = 1e9 + 7;
+const long long INF = 9223372036854775807;
+
+int k;
+
+long long poww(long long a, long long b) {
+    long long res = 1, base = a;
+    while (b) {
+        if (b & 1) {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    //freopen("1.in", "r", stdin);
+    //freopen("1.out", "w", stdout);
+#endif
+    scanf("%d", &k);
+    long long total = (poww(2, k) - 1 + mod) % mod;
+    long long ans = 6 * poww(4, (total - 1 + mod) % mod) % mod;
+    printf("%lld\n", ans);
+    return 0;
+}

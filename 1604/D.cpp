0edc8162b554789fc13e6e523ee1ad04a305779b
kidddddddddddddddddddddd <1062602710@qq.com>
2021-10-31 15:17:59 +0800

    1604 D

---
+++ 1604/D.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+const int maxn = 2e5 + 7;
+const int maxm = 1e6 + 7;
+const int mod = 998244353;
+
+int T;
+int x, y;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("1.in", "r", stdin);
+	// freopen("1.out", "w", stdout);
+#endif
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d%d", &x, &y);
+		printf("%d\n", x <= y ? y - y % x / 2 : x + y);
+	}
+	return 0;
+}

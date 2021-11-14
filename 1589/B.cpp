7efde706a2f78b0ac0659f97cc08b84744f8f9ee
kidddddddddddddddddddddd <1062602710@qq.com>
2021-11-14 23:41:13 +0800

    1589 B

---
+++ 1589/B.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+const int maxn = 2e5 + 7;
+const int maxm = 2e6 + 7;
+const int mod = 998244353;
+
+int T;
+long long n, m;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("1.in", "r", stdin);
+	//  freopen("1.out", "w", stdout);
+#endif
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%lld%lld", &n, &m);
+		printf("%lld\n", n * m / 3 + (n % 3 && m % 3));
+	}
+	return 0;
+}

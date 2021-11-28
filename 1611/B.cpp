3e5d265116719cca1b2feeed4cd91af874868879
kidddddddddddddddddddddd <1062602710@qq.com>
2021-11-28 21:43:14 +0800

    1611 B

---
+++ 1611/B.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+const int maxn = 2e5 + 7;
+const int maxm = 2e6 + 7;
+const int mod = 998244353;
+
+int T;
+int a, b;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("1.in", "r", stdin);
+	//  freopen("1.out", "w", stdout);
+#endif
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", min(min(a, b), (a + b) / 4));
+	}
+	return 0;
+}
